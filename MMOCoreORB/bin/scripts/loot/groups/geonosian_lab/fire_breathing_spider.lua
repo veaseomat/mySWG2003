56c8627df96d8bf8e1b1d6f20d1f205df9d880a4
veaseomat <bricethaice@gmail.com>
2021-03-05 15:09:45 -0600

    loot fixes. naboo weapons facility always imp, rori hyperdrive always reb
    Change-Id: Idb170abcd047e276dc9af85218b3b3d2bb952f44

--- MMOCoreORB/bin/scripts/loot/groups/geonosian_lab/fire_breathing_spider.lua
+++ MMOCoreORB/bin/scripts/loot/groups/geonosian_lab/fire_breathing_spider.lua
@@ -5,7 +5,10 @@ fire_breathing_spider = {
 	minimumLevel = 0,
 	maximumLevel = 0,
 	lootItems = {
-
+		{itemTemplate = "geo_spider_fang", weight = 7500000},
+		{itemTemplate = "geo_spider_venom", weight = 1500000},
+		{itemTemplate = "geo_spider_venom_base", weight = 500000},
+		{itemTemplate = "geo_spider_venom_rare", weight = 500000}
 	}
 }
 
