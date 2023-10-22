6049164f4895b32a9fdd167515f0b063f4effa92
veaseomat <bricethaice@gmail.com>
2023-10-22 16:53:25 -0500

    unstick fixed
    Change-Id: I458fc63f5bbb44fa86ce0bad016dc02250ea819a

--- MMOCoreORB/src/server/zone/objects/creature/commands/UnstickCommand.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/UnstickCommand.h
@@ -5,6 +5,8 @@
 #ifndef UNSTICKCOMMAND_H_
 #define UNSTICKCOMMAND_H_
 
+#include "server/zone/objects/scene/SceneObject.h"
+
 class UnstickCommand : public QueueCommand {
 public:
 
@@ -21,19 +23,55 @@ public:
 		if (!checkInvalidLocomotions(creature))
 			return INVALIDLOCOMOTION;
 
-		if (creature != nullptr)
-			creature->error("used /unstick " + arguments.toString());
+		if (creature->isInCombat())
+			return INVALIDSTATE;
 
-		/*
-string/en/cmd_err.stf	7	unstick_in_progress	Unstick in progress
-string/en/cmd_err.stf	8	unstick_request_complete	Unstick complete
-string/en/cmd_err.stf	9	unstick_request_cancelled	Unstick request was cancelled
-		 */
+		CreatureObject* player = cast<CreatureObject*>(creature);
+		Zone* zone = player->getZone();
 
-		return SUCCESS;
+		if(zone == NULL){
+			return GENERALERROR;
+		}
+
+		if (player->isDead() || player->isIncapacitated()){
+			player->sendSystemMessage("You cant unstick now");
+			return GENERALERROR;
+		}
+
+		if (player->isRidingMount()) {
+			player->sendSystemMessage("You cannot unstick while mounted.");
+			return GENERALERROR;
+		}
+
+		if (player->hasState(CreatureState::PILOTINGSHIP)) {
+			player->sendSystemMessage("You cant unstick while in a ship");
+			return GENERALERROR;
+			}
+
+		if (!player->checkCooldownRecovery("used_unstick")) {
+			StringIdChatParameter stringId;
+
+			Time* cdTime = player->getCooldownTime("used_unstick");
+
+
+			int timeLeft = floor((float)cdTime->miliDifference() / 1000) *-1;
+
+			stringId.setStringId("You must waiting....");
+			stringId.setDI(timeLeft);
+			player->sendSystemMessage("Unstick is on a 1 minute cooldown.");
+			return 0;
+		}
+
+		player->initializePosition(player->getPositionX() + 10, player->getPositionZ() + 10, player->getPositionY() + 10);
+
+		zone->transferObject(player, 1, true);
+
+		player->setPosture(CreaturePosture::UPRIGHT);
+		player->addCooldown("used_unstick", 60000);
+		player->sendSystemMessage("You have been teleported to a safe spot. Wait 15 seconds for recalibration.");
+
+	return SUCCESS;
 	}
-
 };
 
 #endif //UNSTICKCOMMAND_H_
-
